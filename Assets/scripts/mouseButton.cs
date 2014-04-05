5b3fb55a638c9da9173b2eb15b1e4b57389c1ea9
bdickason <dickason@gmail.com>
2014-04-05 15:14:01 -0400

    Can accept touch input

---
+++ Assets/scripts/mouseButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseButton : MonoBehaviour {
+	void OnMouseDown() {
+		// Check to see if mouse is pressed down
+		Debug.Log("Mouse is down on " + this.name + "!");
+	}
+
+	void OnMouseUp() {
+		// Check to see if mouse is pressed down
+		Debug.Log("Mouse is up on " + this.name + "!");
+	}
+
+}
