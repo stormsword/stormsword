46e960643141eb63c338f2f756df5bd511b5e630
bdickason <dickason@gmail.com>
2014-06-28 18:57:18 -0400

    Multiple layers now scroll in parallax

---
+++ Assets/scripts/camera/ScrollingScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* ScrollingScript - Used to scroll a background or static object 
+
+ Usage: Assign to a layer you want to scroll
+
+ isLinkedToCamera (bool) - Does the current object also move the camera? */
+
+public class ScrollingScript : MonoBehaviour {
+
+	public Vector2 speed = new Vector2(2, 2);
+	public Vector2 direction = new Vector2(-1, 0);
+
+	public bool isLinkedToCamera = false;
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 movement = new Vector3(
+			speed.x * direction.x,
+			speed.y * direction.y,
+			0);
+
+		movement *= Time.deltaTime;
+		transform.Translate(movement);
+
+		// Move the Camera
+		if(isLinkedToCamera) {
+			Camera.main.transform.Translate(movement);
+		}
+	}
+}
--- README.md
+++ README.md
@@ -3,6 +3,23 @@ Stormsword
 
 Simple iOS 2d game
 
+# Movement
+
+The player will move with WASD keys and can move freely in all four directions.
+
+It is possible (using abilities, for example) for the player to 'leap' or be tossed in the air.
+
+
+
+# Camera
+
+The camera looks top-down at the player and art will be rendered with a slight lean to give the illusion of depth (think Zelda).
+
+The Camera will pan in all four directions, following the player but creating bounds when the player hits a wall he/she cannot pass. 
+
+Super Mario World Camera Logic Review: https://www.youtube.com/watch?v=TCIMPYM0AQg
+
+
 
 # License
 
