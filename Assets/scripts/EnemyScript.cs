d6e205fcb73302851606547807abdaf283410602
bdickason <dickason@gmail.com>
2014-06-28 13:36:25 -0400

    Enemies can now have multiple weapons

---
+++ Assets/scripts/EnemyScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* EnemyScript - Generic Enemy Behavior */
+
+public class EnemyScript : MonoBehaviour {
+
+	private WeaponScript[] weapons;
+
+	void Awake() {
+		// Grab the weapon once when the enemy spawns
+		weapons = GetComponentsInChildren<WeaponScript>();
+	}
+
+	void Update() {
+		foreach(WeaponScript weapon in weapons) {
+			// Auto-fire
+			if(weapon != null && weapon.CanAttack) {
+				weapon.Attack(true);
+			}
+		}
+	}
+}
