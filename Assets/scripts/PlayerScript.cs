1ea3b050aa35e7808b95ede70151753e6d79d725
bdickason <dickason@gmail.com>
2014-06-28 11:58:33 -0400

    Can move player around the screen

---
+++ Assets/scripts/PlayerScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	// Speed of the player
+	public Vector2 speed = new Vector2(50, 50);
+
+	// Store the player's movement
+	private Vector2 movement;
+	
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Retrieve axis information from controller
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+
+		// Calculate movement per-direction
+		movement = new Vector2(
+			speed.x * inputX,
+			speed.y * inputY);
+	}
+
+	// Physics calcs
+	void FixedUpdate() {
+		// Move the game object
+		rigidbody2D.velocity = movement;
+	}
+}
