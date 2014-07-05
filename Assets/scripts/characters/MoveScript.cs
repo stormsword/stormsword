a3ff18c97d98fbf87574f36891289caa22b06096
bdickason <dickason@gmail.com>
2014-07-05 13:54:49 -0400

    Player properly faces a direction when moving

--- Assets/scripts/characters/MoveScript.cs
+++ Assets/scripts/characters/MoveScript.cs
@@ -29,8 +29,27 @@ public class MoveScript : MonoBehaviour {
 		movement = direction * speed;	// Calculate movement amount
 
 
-		if(movement.x != 0 || movement.y != 0) {
-			facing = new Vector2(Mathf.CeilToInt(direction.x), Mathf.CeilToInt (direction.y));	// Round direction up because direction.x/y can be 0.2 or 0.4, etc.
+		if(direction.x != 0 || direction.y != 0) {
+
+			/* Unity doesn't have a 'always round away from zero' function */
+
+			int x = 0;
+			int y = 0;
+			if(direction.x > 0) {
+				x = 1;
+			}
+			else if(direction.x < 0) {
+				x = -1;
+			}
+			if(direction.y > 0) {
+				y = 1;
+			}
+			else if(direction.y < 0) {
+				y = -1;
+			}
+
+			facing = new Vector2(x, y);	// Round direction up because direction.x/y can be 0.2 or 0.4, etc.
+			Debug.Log (facing);
 			isMoving = true;
 		}
 		else {
