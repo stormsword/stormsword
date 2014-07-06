467f70422f48c5ffaf7507eca7f8ee562115e168
bdickason <dickason@gmail.com>
2014-07-06 15:39:33 -0400

    HP bubble now floats away from an enemy's head when it takes dmg

--- Assets/scripts/characters/HealthScript.cs
+++ Assets/scripts/characters/HealthScript.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 /* HealthScript - Gives an object HP */
 
+public enum dmgTypes { Damage, Heal };
+
 public class HealthScript : MonoBehaviour {
 
 	// Total Hitpoints
@@ -11,6 +13,9 @@ public class HealthScript : MonoBehaviour {
 	// Enemy or Player?
 	public bool isEnemy = true;
 
+	// HP Bubble displayed when a character takes dmg
+	public Transform HPBubble;
+	
 	// Use this for initialization
 	void Start () {
 	
@@ -24,6 +29,17 @@ public class HealthScript : MonoBehaviour {
 	/* Damage - Inflicts damage and check if the object should be destroyed */
 	public void Damage(int damageCount) {
 		hp -= damageCount;
+
+		// Spawn -hp bubble
+		var hpBubble = Instantiate(HPBubble) as Transform;
+		hpBubble.transform.parent = transform;	// Make the current character its' parent
+
+		// Set type of attack
+		var hpBubbleScript = hpBubble.GetComponent<hpBubbleScript>();
+		hpBubbleScript.dmgType = dmgTypes.Damage;
+		hpBubbleScript.amount = damageCount;
+
+		// Handle death
 
 		if (hp <= 0) {
 			// Object is dead
