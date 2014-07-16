2554dd913a9f725177c93f12b4a7565108e60fdf
bdickason <dickason@gmail.com>
2014-07-16 23:28:05 +0200

    HP Bars now decrease as character loses HP

--- Assets/scripts/ui/HealthBar.cs
+++ Assets/scripts/ui/HealthBar.cs
@@ -7,9 +7,18 @@ public class HealthBar : MonoBehaviour {
 	internal CharacterScript character;	// Character to display HP for
 	
 	private HealthScript health;		// Health of above character
+	private GUITexture currentHPBar;	// Remaining HP bar
+	private float currentHPBarStart;	
 
+	// HP Bar Visual Design
+	public int BarWidth = 40;
+	public int BarHeight = 15;
+
+	// Character Stats
 	private int maxHp = 0;
 	private int currentHp = 0;
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -21,6 +30,11 @@ public class HealthBar : MonoBehaviour {
 
 			maxHp = health.hp;	// HP when mob spawns is assumed to be the total hp it can have
 		}
+
+		// Find current HP bar
+		currentHPBar = transform.GetComponentInChildren<GUITexture>();
+		currentHPBar.transform.localScale = Vector3.zero;	// Zero-out the scale vector to allow us to manipulate it later
+	
 	}
 	
 	// Update is called once per frame
@@ -31,6 +45,10 @@ public class HealthBar : MonoBehaviour {
 		transform.position = Camera.main.WorldToViewportPoint(character.transform.position);
 
 		// Update health bar size when character is damanged
-
+		float HPRatio = (float) decimal.Divide(currentHp, maxHp);	// Dividing two ints normally returns an int - this way we get a float
+		float CurrentHPBarWidth = HPRatio * BarWidth;
+		Rect newRect = new Rect(10, 10, CurrentHPBarWidth, BarHeight);
+		currentHPBar.pixelInset = newRect;
 	}
+
 }
