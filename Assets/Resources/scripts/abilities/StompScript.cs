f12144b18d173899dd69cfb92dde658daa448481
bdickason <dickason@gmail.com>
2014-07-26 01:11:47 +0200

    Added stomp placeholder ability

---
+++ Assets/Resources/scripts/abilities/StompScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StompScript : MonoBehaviour {
+
+	private AbilitySlotScript abilitySlot;	// Get the ability slot that this ability is equipped in
+
+	public float cooldown = 2.0f; // Cooldown between abilities
+	public float damage = 1;	// Damage the ability does on impact
+	public float radius = 5; 	// Radius the ability affects upon impact
+
+	// Use this for initialization
+	void Start () {
+		abilitySlot = transform.parent.GetComponent<AbilitySlotScript>();	// Grab the parent ability to get any slot-related info
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	/* Cast - Trigger the current ability (if off cooldown) */
+	internal void Cast() {
+		if(CanCast) {
+			Debug.Log (abilitySlot.abilityCooldown);
+			// Used ability, trigger cooldown
+			abilitySlot.Cooldown(cooldown);
+			Debug.Log (abilitySlot.abilityCooldown);
+//			Debug.Log("Cast stomp!");
+		}
+	}
+
+	// Is the ability ready to be cast?
+	public bool CanCast {
+		get {
+			return(abilitySlot.abilityCooldown <= 0f);	
+		}
+	}
+}
