94f648135210d5f9adf6d68a34f8a063ded5095c
bdickason <dickason@gmail.com>
2014-07-27 17:21:28 +0200

    Consolidated dmg into a single if statement

--- Assets/Resources/scripts/abilities/EffectScript.cs
+++ Assets/Resources/scripts/abilities/EffectScript.cs
@@ -4,9 +4,19 @@ using System.Collections;
 public class EffectScript : MonoBehaviour {
 
 	public float duration = 0.5f;
+	private float radius = 0f;
+
+	private CircleCollider2D stompCollider;	// Collider on the stomp effect
+
+	private StompScript stompScript;
 
 	// Use this for initialization
 	void Start () {
+		stompCollider = GetComponent<CircleCollider2D>();
+		stompScript = transform.parent.GetComponent<StompScript>();
+
+		stompCollider.radius = stompScript.radius;	// Stomp effect should always match radius of Stomp
+
 		Destroy (gameObject, duration);	// effect should go away after <duration>
 	}
 	
