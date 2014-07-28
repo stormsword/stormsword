4d5c858bdd3f965294c36194316e02c18029039a
bdickason <dickason@gmail.com>
2014-07-28 19:12:55 +0200

    Added base Effect script which can be extended to create new effects

---
+++ Assets/Resources/scripts/effects/Effect.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* Effect - Extend this to create a buff/debuff that affects a character 
+ e.g. if you want to reduce a character's movement speed by 20% or apply a poison
+ */
+
+public class Effect : MonoBehaviour {
+
+	public float duration;		// How long (seconds) should the effect last
+	public float startTime;		// How long (seconds) until the effect starts
+	public float tick;			// How long (seconds) between each pulse of the effect
+
+	internal GameObject target;	// The target the effect is affecting
+	
+	void Start () {
+		// Apply the affect
+		InvokeRepeating("ApplyEffect", startTime, tick);	// Apply the affect every 'tick' seconds
+
+		Invoke ("EndEffect", duration);			// In 'duration' seconds, destroy the effect
+	}
+
+	protected virtual void ApplyEffect() {
+		// Extend this function to add additional logic upon application (i.e. set movespeed -20%)
+	}
+
+	protected virtual void EndEffect() {
+		// Extend this function to add additional logic upon removal (i.e. return movespeed to 100%)
+
+		CancelInvoke();			// Stop any existing functions
+		Destroy(gameObject);	// Destroy the game object (removing the affect)
+	}
+}
