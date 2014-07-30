e6bbca5a600c6e57f5dcdc21eeb9e3f7e3c761f6
bdickason <dickason@gmail.com>
2014-07-30 18:26:45 +0200

    Added ability icon to screen

---
+++ Assets/Resources/scripts/abilities/AbilityUIScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* AbilityUIScript - places ability UI elements on the screen */
+public class AbilityUIScript : MonoBehaviour {
+
+	public int x;					// X coordinate to display the image at
+	public int y;					// Y coordinate to display the image at
+	public int width;				// Width of the image
+	public int height;				// Height of the image
+
+
+	public Texture2D abilityImage;	// The image to display for this ability
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnGUI() {
+		GUI.Label(new Rect(x, y, width, height), abilityImage);
+	}
+}
