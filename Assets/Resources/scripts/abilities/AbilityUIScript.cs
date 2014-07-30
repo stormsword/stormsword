6aa2c6d74877eb09bdbfe5b28a55426f1216571d
bdickason <dickason@gmail.com>
2014-07-30 18:46:22 +0200

    Ability UI icon now fades out when not available

--- Assets/Resources/scripts/abilities/AbilityUIScript.cs
+++ Assets/Resources/scripts/abilities/AbilityUIScript.cs
@@ -8,9 +8,13 @@ public class AbilityUIScript : MonoBehaviour {
 	public int y;					// Y coordinate to display the image at
 	public int width;				// Width of the image
 	public int height;				// Height of the image
-
-
+	
 	public Texture2D abilityImage;	// The image to display for this ability
+
+	public float fadeAlpha = 0.6f;
+
+	private float currentAlpha = 1f;		// Current Alpha (opacity) value of the image
+
 
 	// Use this for initialization
 	void Start () {
@@ -18,6 +22,23 @@ public class AbilityUIScript : MonoBehaviour {
 	}
 	
 	void OnGUI() {
+		Color tmpColor = GUI.color;
+		GUI.color = new Color(tmpColor.r, tmpColor.g, tmpColor.b, currentAlpha);
 		GUI.Label(new Rect(x, y, width, height), abilityImage);
+		GUI.color = tmpColor;
+	}
+
+	/* FadeOut - Called to fade the current UI element to partial opacity
+	 	e.g. when it's not available */
+	internal void FadeOut() {
+
+		currentAlpha = fadeAlpha;
+		Debug.Log ("Fading out: " + currentAlpha);
+	}
+
+	/* FadeIn - Called to fade the current UI element to full opacity */
+	internal void FadeIn() {
+		currentAlpha = 1f;
+		Debug.Log ("Fading in: " + currentAlpha);
 	}
 }
