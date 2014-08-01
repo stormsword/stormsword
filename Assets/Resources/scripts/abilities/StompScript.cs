07acbe7b123555332c3810f950dffcd28649aa18
bdickason <dickason@gmail.com>
2014-08-01 15:40:43 +0200

    Adjusted radius of stomp collider to fit the animation

--- Assets/Resources/scripts/abilities/StompScript.cs
+++ Assets/Resources/scripts/abilities/StompScript.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class StompScript : AbilityScript {
 	
-  public float snareAmount = 0f;    // Percentage (%) to snare affected targets (0-100)
-  public float snareDuration = 0f;  // Time (seconds) snare the effect lasts
-		
+//  public float snareAmount = 0f;    // Percentage (%) to snare affected targets (0-100)
+//  public float snareDuration = 0f;  // Time (seconds) snare the effect lasts
 }
