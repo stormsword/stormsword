7ebf40632b01c73bc33aa3cded3f42621fa14094
bdickason <dickason@gmail.com>
2014-08-01 15:33:49 +0200

    Renamed 'stun' effect to 'knockback' effect to be more accurate

--- Assets/Resources/scripts/effects/KnockbackEffect.cs
+++ Assets/Resources/scripts/effects/KnockbackEffect.cs
@@ -1,9 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-/* Stun Effect - Knocks back a character away from the current character and stuns them */
+/* Stun Effect - Knocks back a character away from the current character */
 
-public class StunEffect : Effect {
+public class KnockbackEffect : Effect {
 	public float amount;	// Amount (#) multiplier on the movement vector
 
 	private MoveScript targetMoveScript;	// The target's movement script
