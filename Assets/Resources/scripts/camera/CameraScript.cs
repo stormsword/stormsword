e1cb7d3b3b5cb10aea38bb968a5981f36bd4837c
bdickason <dickason@gmail.com>
2014-08-02 23:03:08 +0200

    Fixed a bug where camera would throw errors if the player died

--- Assets/Resources/scripts/camera/CameraScript.cs
+++ Assets/Resources/scripts/camera/CameraScript.cs
@@ -9,27 +9,30 @@ public class CameraScript : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		this.target = GameObject.FindGameObjectWithTag("Player").transform;
-		Debug.Log(this.target);
+		target = GameObject.FindGameObjectWithTag("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 point = camera.WorldToViewportPoint(target.position);
-		
-		float cameraMovementX = 0.5f;
-		float cameraMovementY = 0.5f;
-		
-		if ((target.position.x - 3f) < 0) {
-			cameraMovementX = point.x;
+		if(target) {
+			// Only track if player is still alive
+			Vector3 point = camera.WorldToViewportPoint(target.position);	// Player's current position
+			
+			float cameraMovementX = 0.5f;
+			float cameraMovementY = 0.5f;
+			
+			if ((target.position.x - 3f) < 0) {
+				cameraMovementX = point.x;
+				Debug.Log (cameraMovementX);
+			}
+			
+			if ((target.position.y - 2.8f) < 0) {
+				cameraMovementY = point.y;
+			}
+			
+			Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(cameraMovementX, cameraMovementY, point.z));
+			Vector3 destination = transform.position + delta;
+			this.transform.position = Vector3.SmoothDamp (this.transform.position, destination, ref velocity, dampTime);
 		}
-		
-		if ((target.position.y - 2.8f) < 0) {
-			cameraMovementY = point.y;
-		}
-		
-		Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(cameraMovementX, cameraMovementY, point.z));
-		Vector3 destination = transform.position + delta;
-		this.transform.position = Vector3.SmoothDamp (this.transform.position, destination, ref velocity, dampTime);
 	}
 }
