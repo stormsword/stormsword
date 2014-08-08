95c845424eb571873ab15c0de216ab4603e0fdab
bdickason <dickason@gmail.com>
2014-08-08 20:42:06 +0200

    Added new camera mode: Menu which pans around the map

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* MainMenuScript - Display main menu and allow player to start game (or continue.... one day!) */
+public class MainMenuScript : MonoBehaviour {
+
+	internal static MainMenuScript instance;
+
+	// Main menu title
+	public Rect titlePosition;
+	public string titleText;
+
+	// Start Game Button
+	public Rect startPosition;
+	public string startText;
+
+	// Continue Game Button
+	public Rect continuePosition;
+	public string continueText;
+
+	void Start (){
+		instance = this;
+	}
+
+	void OnGui() {
+		// Draw black background over scene
+		GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "");
+
+		// Draw title text/image
+		GUI.Label(titlePosition, titleText);
+
+		// Draw start button
+		GUI.Button(startPosition, startText);
+
+		// Draw continue button
+		GUI.Button(continuePosition, continueText);
+	}
+	
+	void Update () {
+	
+	}
+}
--- Assets/Resources/scripts/camera/CameraScript.cs
+++ Assets/Resources/scripts/camera/CameraScript.cs
@@ -1,21 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum CameraModes {
+	FollowPlayer,
+	Menu
+}
+
 public class CameraScript : MonoBehaviour {
-	
+
+	public CameraModes cameraMode;
 	public float dampTime = 0.15f;
 	private Vector3 velocity = Vector3.zero;
 	private Transform target;
-	
+
 	// Use this for initialization
 	void Start () {
-		target = GameObject.FindGameObjectWithTag("Player").transform;
-		this.transform.position = target.position;	// Snap to player's position when the game starts
+		if(cameraMode == CameraModes.FollowPlayer) {
+			target = GameObject.FindGameObjectWithTag("Player").transform;
+			this.transform.position = target.position;	// Snap to player's position when the game starts
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(target) {
+		if(target && (cameraMode == CameraModes.FollowPlayer)) {
 			// Only track if player is still alive
 			Vector3 point = camera.WorldToViewportPoint(target.position);	// Player's current position
 			
@@ -25,6 +33,9 @@ public class CameraScript : MonoBehaviour {
 			Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(cameraMovementX, cameraMovementY, point.z));
 			Vector3 destination = transform.position + delta;
 			this.transform.position = Vector3.SmoothDamp (this.transform.position, destination, ref velocity, dampTime);
+		}
+		else if(cameraMode == CameraModes.Menu) {
+			// Slowly pan to the right in a semi-cinematic fashion
 		}
 	}
 }
