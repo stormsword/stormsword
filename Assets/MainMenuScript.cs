a0a22be35ac23748019bb10d681397efbfa1c4f7
bdickason <dickason@gmail.com>
2014-08-08 21:49:38 +0200

    Start button can be clicked

--- Assets/MainMenuScript.cs
+++ Assets/MainMenuScript.cs
@@ -1,43 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-/* MainMenuScript - Display main menu and allow player to start game (or continue.... one day!) */
+/* MainMenuScript - Controller for main menu functionality
+ 	Allows player to start the game (or continue.... one day!) */
 public class MainMenuScript : MonoBehaviour {
 
-	internal static MainMenuScript instance;
+	public static MainMenuScript instance;
 
-	// Main menu title
-	public Rect titlePosition;
-	public string titleText;
-
-	// Start Game Button
-	public Rect startPosition;
-	public string startText;
-
-	// Continue Game Button
-	public Rect continuePosition;
-	public string continueText;
-
-	void Start (){
-		instance = this;
+	void Awake() {
+		instance = this;	// Allows us to access MainMenuScript from anywhere 
+							// (which is ok because there will only ever be one of them active at a time)
 	}
-
-	void OnGUI() {
-		// Draw black background over scene
-		GUI.backgroundColor = Color.black;
-		GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "");
-
-		// Draw title text/image
-		GUI.Label(titlePosition, titleText);
-
-		// Draw start button
-		GUI.Button(startPosition, startText);
-
-		// Draw continue button
-		GUI.Button(continuePosition, continueText);
-	}
-	
+	// Update is called once per frame
 	void Update () {
-	
+		
+	}
+
+	public void StartGame() {
+		Debug.Log ("Starting game!");
+	}
+
+	public void ContinueGame() {
+		Debug.Log ("Player wants to continue!");
 	}
 }
---
+++ Assets/Resources/scripts/ui/MainMenuUIScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* MainMenuScript - Display UI for main menu */
+public class MainMenuUIScript : MonoBehaviour {
+
+	internal static MainMenuUIScript instance;
+
+	// Main menu title
+	public Rect titlePosition;
+	public string titleText;
+
+	// Start Game Button
+	public Rect startPosition;
+	public string startText;
+
+	// Continue Game Button
+	public Rect continuePosition;
+	public string continueText;
+
+	void Start (){
+		instance = this;
+	}
+
+	void OnGUI() {
+		// Draw black background over scene
+		GUI.backgroundColor = Color.black;
+		GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "");
+
+		// Draw title text/image
+		GUI.Label(titlePosition, titleText);
+
+		// Draw start button
+		GUI.contentColor = Color.white;
+		bool startButton = GUI.Button(startPosition, startText);
+
+		if(startButton) {
+			// Player pressed the start button
+			Debug.Log ("Got here");
+			MainMenuScript.instance.StartGame();
+		}
+		
+		// Draw continue button
+		GUI.contentColor = Color.grey;
+		GUI.Label(continuePosition, continueText);	// Currently continue does not work.
+
+
+	}
+}
