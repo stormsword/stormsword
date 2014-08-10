968111246bd0f7bcbac01356afe3016797900f69
bdickason <dickason@gmail.com>
2014-08-10 19:21:31 -0400

    Added a slightly more clear comment

--- Assets/Resources/scripts/tracking/SceneScript.cs
+++ Assets/Resources/scripts/tracking/SceneScript.cs
@@ -6,13 +6,12 @@ public class SceneScript : MonoBehaviour {
 
 	private TiledMapComponent mapComponent;
 
-	// Use this for initialization
+	// Scene has loaded, send data so we know how far a player gets
 	void Start () {
 		mapComponent = GetComponent<TiledMapComponent>();
 		if(mapComponent) {
 			TrackingScript.instance.AddProperty("scene", mapComponent.MapTMX.name);
 			TrackingScript.instance.Track("scene loaded");
 		}
-	
 	}
 }
