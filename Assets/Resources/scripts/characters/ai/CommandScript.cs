93cb75a54b4c04a5c735c581ced0b8a64c3b2840
bdickason <dickason@gmail.com>
2014-08-19 23:07:04 -0400

    Added incredibly simple command stack

---
+++ Assets/Resources/scripts/characters/ai/CommandScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CommandScript : MonoBehaviour {
+		
+	// Use this for initialization
+	void Start() {
+	}
+	
+	void Update() {
+	}
+}
---
+++ Assets/Resources/scripts/characters/ai/CommandStackScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/* CommandStackScript - A Stack of commands that allow an enemy to figure out what he/she/it should be doing at any given time */
+public class CommandStackScript : MonoBehaviour {
+	
+	private Stack<CommandScript> commands;
+	
+	// Use this for initialization
+	void Start () {
+		commands = new Stack<CommandScript>();
+		
+		CommandScript command = new CommandScript();
+		
+		commands.Push(command);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (commands.ToString());
+	}
+}
