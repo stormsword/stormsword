75159accabfed9218968dcdc7d59c80f0e00b160
bdickason <dickason@gmail.com>
2014-08-20 19:56:15 -0400

    Enemies now chase player around the map forever

--- Assets/Resources/scripts/characters/ai/MoveCommandScript.cs
+++ Assets/Resources/scripts/characters/ai/MoveCommandScript.cs
@@ -9,14 +9,24 @@ public class MoveCommandScript : CommandScript {
 	/* MoveCommandScript - Constructur called when Command is first created 
 	 _character - the Character that the Command Stack is attached to */
 	public MoveCommandScript(GameObject _character) : base(_character) {
-
+		moveScript = character.GetComponent<MoveScript>();
 	}
 
 	/* Execute is usually called once per frame */
 	public override void Execute () {
 		if(this.isActive) {
-			Debug.Log("Moving to....");
-			Debug.Log (direction);
+			if(target != null) {
+				destination = GetDestination(target);
+				moveScript.Move(destination);
+			}
 		}
+	}
+
+	private Vector2 GetDestination(GameObject target) {
+		return(target.transform.position - character.transform.position);
+	}
+
+	private float GetDistance(GameObject target) {
+		return(Vector2.Distance(character.transform.position, target.transform.position));
 	}
 }
