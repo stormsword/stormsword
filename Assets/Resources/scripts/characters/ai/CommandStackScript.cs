4743d6ee07369a9fe98259edf16abc4be60ee472
bdickason <dickason@gmail.com>
2014-08-20 22:41:50 -0400

    Enemies properly wander or charge based on archetype setting

--- Assets/Resources/scripts/characters/ai/CommandStackScript.cs
+++ Assets/Resources/scripts/characters/ai/CommandStackScript.cs
@@ -37,13 +37,11 @@ public class CommandStackScript {
 				// Pause current command
 				currentCommand.Pause();
 				commands.Push(command);
-				Debug.Log (command.GetType());
 			}
 		}
 		else {
 			// The stack is empty so push it!
 			commands.Push(command);
-			Debug.Log (command.GetType());
 		}
 	}
 
