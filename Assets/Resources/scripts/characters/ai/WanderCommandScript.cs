07931a64b3ea673071629bcaee0d36b1d6f52033
bdickason <dickason@gmail.com>
2014-08-20 22:17:09 -0400

    Enemies now only charge when you enter their agro radius

---
+++ Assets/Resources/scripts/characters/ai/WanderCommandScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* WanderCommand - Wanders randomly around the current position */
+public class WanderCommandScript : CommandScript {
+
+	MoveScript moveScript;
+
+	/* MoveCommandScript - Constructur called when Command is first created 
+	 _character - the Character that the Command Stack is attached to */
+	public WanderCommandScript(GameObject _character) : base(_character) {
+		moveScript = character.GetComponent<MoveScript>();
+	}
+
+	/* Execute is usually called once per frame */
+	public override void Execute () {
+		if(this.isActive) {
+			if(target != null) {
+				destination = GetRandomDestination();
+				moveScript.Move(destination);
+			}
+		}
+	}
+
+	private Vector2 GetRandomDestination() {
+		return(Random.insideUnitCircle * 5);
+	}	
+}
