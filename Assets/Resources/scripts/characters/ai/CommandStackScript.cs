d4ed471ca1b616dd42368462230170a015e7b7f5
bdickason <dickason@gmail.com>
2014-08-20 22:09:15 -0400

    All enemies charge at player by default

--- Assets/Resources/scripts/characters/ai/CommandStackScript.cs
+++ Assets/Resources/scripts/characters/ai/CommandStackScript.cs
@@ -7,7 +7,7 @@ public class CommandStackScript {
 	
 	// The data structure of commands
 	private Stack<CommandScript> commands;
-
+	
 	/* Constructor - Creates a new CommandStack */
 	public CommandStackScript() {
 		commands = new Stack<CommandScript>();
@@ -17,17 +17,32 @@ public class CommandStackScript {
 	public void Execute () {
 		// Run the given command this frame
 		if(currentCommand != null) {
-			currentCommand.Execute();
+			if(currentCommand.isComplete) {
+				// Current task is finished, pop it off!
+				Debug.Log ("Current command is complete!");
+				commands.Pop();
+			}
+			else {
+				currentCommand.Execute();
+			}
 		}
 	}
+
 
 	/* Add - Adds a command to the top of the stack, pausing the current item and executing it next frame */
 	public void Add(CommandScript command) {
 		if(commands.Count > 0) {
-			// Pause current command
-			currentCommand.Pause();
+			// Make sure we're not just receiving the same action again
+			if(currentCommand.GetType() != command.GetType()) {
+				// Pause current command
+				currentCommand.Pause();
+				commands.Push(command);
+			}
 		}
-		commands.Push(command);
+		else {
+			// The stack is empty so push it!
+			commands.Push(command);
+		}
 	}
 
 	/* Remove - Pops the top command off the stack */
@@ -42,7 +57,6 @@ public class CommandStackScript {
 		}
 
 	}
-
 	public CommandScript currentCommand {
 		get {
 			if(commands.Count > 0) {
@@ -54,4 +68,6 @@ public class CommandStackScript {
 			}
 		}
 	}
+
+
 }
