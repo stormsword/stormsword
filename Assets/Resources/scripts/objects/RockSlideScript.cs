8b4c72112aa4a5268938f4356aa593d051c43bbc
bdickason <dickason@gmail.com>
2014-08-24 22:09:26 -0400

    Added ability to have the rocklside trigger once or multiple times

--- Assets/Resources/scripts/objects/RockSlideScript.cs
+++ Assets/Resources/scripts/objects/RockSlideScript.cs
@@ -3,26 +3,29 @@ using System.Collections;
 
 public class RockSlideScript : MonoBehaviour {
 
-
+	[Tooltip("The Game Object to spawn when the player enters the trigger")]
 	public Transform RockSlide;
+
+	[Tooltip("Should this only happen once? Or many times?")]
+	public bool onlyOnce = true;
+
+	private BoxCollider2D trigger;
 
 	// Use this for initialization
 	void Start () {
+		trigger = GetComponent<BoxCollider2D>();
 	}
 
 	void OnTriggerEnter2D(Collider2D defender) {
 		// Make sure it's a player that triggers the collision
 		if(defender.tag == "Player") {
-
-			Debug.Log("Player entered rockslide area!");
 			// Spawn Rockslide object
 			GameObject rockslide = Instantiate(RockSlide) as GameObject;
 
-			// When boss is dead, destroy it
+			if(onlyOnce) {
+				// Disable this trigger because the rockslide should only trigger once
+				trigger.enabled = false;
+			}
 		}
-	}
-
-	void SpawnRockslide() {
-
 	}
 }
