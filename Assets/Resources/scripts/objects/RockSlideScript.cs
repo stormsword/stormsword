6e1c37133d1e0f8cc5612a71b92a49d805ef0d59
bdickason <dickason@gmail.com>
2014-08-24 20:47:47 -0400

    Added basic rockslide trigger to trap player in the room

---
+++ Assets/Resources/scripts/objects/RockSlideScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockSlideScript : MonoBehaviour {
+
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+//		player = GameObject.FindGameObjectWithTag('Player');		
+	}
+
+	void OnTriggerEnter2D(Collider2D defenderCollider) {
+		// Spawn Rockslide object
+		// Make usre player can't attakc it
+		// When boss is dead, destroy it	
+	}
+}
